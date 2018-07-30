3c403c3f3e9cd8e14881830b5da6b2b315bc19cc
wrazik <wojciech.razik@thaumatec.com>
2018-07-30 13:39:23 +0200

    Added test framework & travis integration

---
+++ volonteerworker_test.js
@@ -0,0 +1,8 @@
+const volounteerworker = require('./volonteerworker.js');
+const { expect } = require('chai');
+
+describe('aaa', () => {
+  it('shouldReturnFalse', () => {
+    expect(true).to.be.false;
+  });
+});
